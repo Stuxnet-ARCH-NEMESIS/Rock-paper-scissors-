edee0bec25d55f9e67f85fc9df5f40b242b807cf
Stuxnet-ARCH-NEMESIS <141284780+Stuxnet-ARCH-NEMESIS@users.noreply.github.com>
2025-11-08 15:01:43 +1100

    sigma

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-    let humanScore = 0
-    let computerScore = 0
+let humanScore = 0
+let computerScore = 0
+let rock = document.querySelector("#rock")
+let scissor = document.querySelector("#scissor")
+let paper = document.querySelector("#paper")
+let btn = document.querySelectorAll("button")
+let div = document.querySelector("div")
+let humanChoice
+let gameOver = false
+
 
 function getComputerChoice () {
                  let min = 1;
@@ -8,62 +16,130 @@ function getComputerChoice () {
                  return computerChoice
                   }
 
-function getHumanChoice () {
-               let humanChoice = prompt("Enter your choice")
-                     humanChoice = humanChoice.toLowerCase()
-               switch (humanChoice)
-               {
-                  case "rock" : humanChoice = 1 ; break
-                  case "scissor" : humanChoice = 2 ; break
-                  case "paper" : humanChoice = 3 ; break
-               }
-               return humanChoice
-               }
+
+scissor.addEventListener("click", function () {
+   humanChoice = 2
+   playRound()
+})
+rock.addEventListener("click", function () {
+   humanChoice = 1
+   playRound()
+})
+
+paper.addEventListener("click", function () {
+   humanChoice = 3
+   playRound()
+})
+      
+              
 
 function playRound () {
-    let humanChoice = getHumanChoice()
+   if (gameOver) return
+   if (gameOver) return
+   if (computerScore === 5 || humanScore ===5) return
+      if (computerScore === 5) {
+         let span = document.createElement("span")
+         span.textContent = 'Computer Won!'
+         div.innerHTML = ""
+         div.appendChild(span)
+         gameOver = true
+         return
+      }
+      else if (humanScore === 5){
+         let span = document.createElement("span")
+         span.textContent = 'Human Won!'
+         div.innerHTML = ""
+         div.appendChild(span)
+         gameOver = true
+         return
+      }
     let computerChoice = getComputerChoice()
     console.log(computerChoice)
      if (humanChoice === 1 && computerChoice === 2) {
         humanScore = humanScore + 1
-        console.log(" Congratulations! You won! Rock beats scissors! Human Score = " + humanScore + " Computer Score = " + computerScore)
+        let span = document.createElement("span")
+        span.textContent = ' Congratulations! You won! Rock beats scissors! Human Score = ' + humanScore + ' Computer Score = ' + computerScore
+        div.innerHTML = ""
+        div.appendChild(span)
+
      }
      else if (humanChoice === 2 && computerChoice === 3) {
         humanScore = humanScore + 1
-        console.log(" Congratulations! You won! Scissors beats paper! Human Score = " + humanScore + " Computer Score = " + computerScore)
+        let span = document.createElement("span")
+        span.textContent = ' Congratulations! You won! Scissors beats paper! Human Score = ' + humanScore + ' Computer Score = ' + computerScore
+        div.innerHTML = ""
+        div.appendChild(span)
      }
      else if (humanChoice === 3 && computerChoice === 1) {
         humanScore = humanScore + 1
-        console.log(" Congratulations! You won! Paper beats rock! Human Score = " + humanScore + " Computer Score = " + computerScore)
+        let span = document.createElement("span")
+        span.textContent = ' Congratulations! You won! Paper beats rock! Human Score = ' + humanScore + ' Computer Score = ' + computerScore
+        div.innerHTML = ""
+        div.appendChild(span)
      }
      else if (humanChoice === 2 && computerChoice === 1) {
         computerScore = computerScore + 1
-        console.log (" Boo! You lost! Rock beats scissors! Human Score = " + humanScore + " Computer Score = " + computerScore)
+        let span = document.createElement("span")
+        span.textContent = 'Boo! You lost! Rock beats scissors! Human Score = ' + humanScore + 'Computer Score = ' + computerScore
+        div.innerHTML = ""
+        div.appendChild(span)
      }
      else if (humanChoice === 3 && computerChoice === 2) {
         computerScore = computerScore + 1
-        console.log (" Boo! You lost! Scissors beats paper! Human Score = " + humanScore + " Computer Score = " + computerScore)
+        let span = document.createElement("span")
+        span.textContent = 'Boo! You lost! Scissors beats paper! Human Score = ' + humanScore + 'Computer Score = ' + computerScore
+        div.innerHTML = ""
+        div.appendChild(span)
      }
      else if (humanChoice === 1 && computerChoice === 3) {
         computerScore = computerScore + 1
-        console.log (" Boo! You lost! Paper beats rock! Human Score = " + humanScore + " Computer Score = " + computerScore)
+        let span = document.createElement("span")
+        span.textContent = 'Boo! You lost! Paper beats rock! Human Score = ' + humanScore + ' Computer Score = ' + computerScore
+        div.innerHTML = ""
+        div.appendChild(span)
       }
      else if (humanChoice === computerChoice) {
-         console.log("Haha! You drew!")
+      let span = document.createElement("span")
+      span.textContent = 'Haha! You drew! Human Score = ' + humanScore + " Computer Score = " + computerScore
+      div.innerHTML = ""
+      div.appendChild(span)
+      }
+      if (computerScore === 5 || humanScore ===5) return
+      if (computerScore === 5) {
+         let span = document.createElement("span")
+         span.textContent = 'Computer Won!'
+         div.innerHTML = ""
+         div.appendChild(span)
+         gameOver = true
+         return
+      }
+      else if (humanScore === 5){
+         let span = document.createElement("span")
+         span.textContent = 'Human Won!'
+         div.innerHTML = ""
+         div.appendChild(span)
+         gameOver = true
+         return
       }
 }
 
-function playGame () {
-   playRound();
+if (computerScore === 5) {
+         let span = document.createElement("span")
+         span.textContent = 'Computer Won!'
+         div.innerHTML = ""
+         div.appendChild(span)
+      }
+      else if (humanScore === 5){
+         let span = document.createElement("span")
+         span.textContent = 'Human Won!'
+         div.innerHTML = ""
+         div.appendChild(span)
+      }
 
-   playRound();
-   playRound();
-   playRound();
-   playRound();
 
-}
 
-playGame()
+
+
 
    
     
